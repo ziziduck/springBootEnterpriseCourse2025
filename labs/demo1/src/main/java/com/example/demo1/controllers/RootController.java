0794bf37637978a7feed8dc1f0c6ceabc4a0d749
ziziduck <tsunglian@gmail.com>
2025-07-21 15:00:20 +0800

    使用物件的存取

--- labs/demo1/src/main/java/com/example/demo1/controllers/RootController.java
+++ labs/demo1/src/main/java/com/example/demo1/controllers/RootController.java
@@ -1,5 +1,6 @@
 package com.example.demo1.controllers;
 
+import com.example.demo1.bean.Course;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +8,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RootController {
 	private static final String MESSAGE1 = "message";
+	private static final String MESSAGE2 = "message2";
+	private static final String INFO_KEY1 = "jcourse";
 
 	@GetMapping("/")
 	public String home(Model model) {
 		model.addAttribute(MESSAGE1, "Hi, I am Mark");
+		model.addAttribute(MESSAGE2, "learning spring");
 		return "home";
 	}
 
 	@GetMapping("/info")
-	public String info() {
+	public String info(Model model) {
+		Course c = new Course();
+		c.setId("POOP");
+		c.setName("Python and OOP programming");
+		c.setDuration(42);
+		c.setFee(28000);
+		model.addAttribute(INFO_KEY1, c);
 		return "info/contact";
 	}
 }
