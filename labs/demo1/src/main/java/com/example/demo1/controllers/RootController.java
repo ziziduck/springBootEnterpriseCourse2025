3f689e181b64ee5b21336b1e22ece4a3a777112e
ziziduck <tsunglian@gmail.com>
2025-07-21 14:06:19 +0800

    傳入字串至template中
    使用${}將變數顯示

--- labs/demo1/src/main/java/com/example/demo1/controllers/RootController.java
+++ labs/demo1/src/main/java/com/example/demo1/controllers/RootController.java
@@ -1,12 +1,21 @@
 package com.example.demo1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RootController {
+	private static final String MESSAGE1 = "message";
+
 	@GetMapping("/")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute(MESSAGE1, "Hi, I am Mark");
 		return "home";
+	}
+
+	@GetMapping("/info")
+	public String info() {
+		return "info/contact";
 	}
 }
