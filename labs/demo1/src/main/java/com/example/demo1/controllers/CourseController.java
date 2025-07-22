1b54c2c03e3d596dc8f267b866975dfb929bdef2
ziziduck <tsunglian@gmail.com>
2025-07-22 13:50:34 +0800

    驗證成功轉導至其它頁面
    使用redirect

--- labs/demo1/src/main/java/com/example/demo1/controllers/CourseController.java
+++ labs/demo1/src/main/java/com/example/demo1/controllers/CourseController.java
@@ -1,9 +1,11 @@
 package com.example.demo1.controllers;
 
 import com.example.demo1.form.CourseForm;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,9 +16,9 @@ public class CourseController {
 
 	@GetMapping("/course")
 	public String showCourseForm(CourseForm f, Model model) {
-		f.setCourseId("OO-226");
-		f.setCourseName("Java and UML");
-		f.setDuration(35);
+//        f.setCourseId("OO-226");
+//        f.setCourseName("Java and UML");
+//        f.setDuration(35);
 		model.addAttribute(COURSE_FORM2, new CourseForm(
 				"BDPY", "Python and Big Data", 42
 		));
@@ -24,8 +26,14 @@ public class CourseController {
 	}
 
 	@PostMapping("/course")
-	public String handleFormSubmit(CourseForm f) {
-		log.info("get course={}", f);
-		return "courseForm"; // but need to refactor later
+	public String handleFormSubmit(@Valid CourseForm f, BindingResult result) {
+		// temporary get the result, see if validation is valid or not
+		if (result.hasErrors()) {
+			return "courseForm";
+		} else {
+			return "home";
+		}
+//        log.info("get course={}", f);
+//        return "courseForm"; // but need to refactor later
 	}
 }
