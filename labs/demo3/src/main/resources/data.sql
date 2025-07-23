ef053cc80c10e5b8366130cc3f979a716b640091
ziziduck <tsunglian@gmail.com>
2025-07-23 15:04:01 +0800

    將SQL移到外面

---
+++ labs/demo3/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO USERS2(username, password) VALUES ('Mark','pwd1');
+INSERT INTO USERS2(username, password) VALUES ('Ken','pwd2');
+INSERT INTO USERS2(username, password) VALUES ('Peter','pwd3');
+INSERT INTO USERS2(username, password) VALUES ('Tim','pwd4');
