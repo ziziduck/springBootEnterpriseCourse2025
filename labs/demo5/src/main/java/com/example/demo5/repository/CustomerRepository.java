2d58d434250bac6a40129eedb7ba739c0f089028
ziziduck <tsunglian@gmail.com>
2025-07-24 15:56:11 +0800

    建立抽象service層讓JPA連線到Restful

--- labs/demo5/src/main/java/com/example/demo5/repository/CustomerRepository.java
+++ labs/demo5/src/main/java/com/example/demo5/repository/CustomerRepository.java
@@ -2,6 +2,11 @@ package com.example.demo5.repository;
 
 import com.example.demo5.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    @Query("SELECT x FROM Customer x ORDER BY x.lastName, x.firstName")
+    List<Customer> findAllOrderByName();
 }
