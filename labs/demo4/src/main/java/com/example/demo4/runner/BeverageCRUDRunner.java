f62514c5bb4511c7112bf915c0285fccb1466477
ziziduck <tsunglian@gmail.com>
2025-07-24 09:38:07 +0800

    BeverageCRUDRunner中實作新增，刪除，修改查詢

--- labs/demo4/src/main/java/com/example/demo4/runner/BeverageCRUDRunner.java
+++ labs/demo4/src/main/java/com/example/demo4/runner/BeverageCRUDRunner.java
@@ -8,6 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 @Component
 @Slf4j
 @Order(1)
@@ -17,8 +20,35 @@ public class BeverageCRUDRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        log.info("repository={}",repository);
+        log.info("repository={}", repository);
         initDB();
+        dumpDB("after insert some data");
+        modifyDB();
+        dumpDB("after modify 3rd beverage");
+        deleteDB();
+        dumpDB("after delete 1st and 3rd beverage");
+
+    }
+
+    private void deleteDB() {
+//        repository.deleteById(1L);
+//        repository.deleteById(3L);
+        repository.deleteAllById(Arrays.asList(1L, 3L));
+    }
+
+    private void modifyDB() {
+        Optional<Beverage> optionalBeverage = repository.findById(3L);
+        if (optionalBeverage.isPresent()) {
+            Beverage beverage = optionalBeverage.get();
+            beverage.setTitle("Venti Hot Americano");
+            repository.save(beverage);
+        }
+    }
+
+    private void dumpDB(String prompt) {
+        for (Beverage b : repository.findAll()) {
+            log.info("after {}, get a beverage={}", prompt, b);
+        }
     }
 
     private void initDB() {
@@ -32,7 +62,19 @@ public class BeverageCRUDRunner implements CommandLineRunner {
         b2.setDetail("3 shot espresso+70% hot milk+...");
         b2.setPrice(200);
         b2.setSugar(0.0f);
+        Beverage b3 = new Beverage();
+        b3.setTitle("Tall Hot Coffee");
+        b3.setDetail("hot americano...");
+        b3.setPrice(130);
+        b3.setSugar(0.0f);
+        Beverage b4 = new Beverage();
+        b4.setTitle("Ice Grande Latte");
+        b4.setDetail("3 shot espresso+60% fresh milk+ice");
+        b4.setPrice(210);
+        b4.setSugar(0.0f);
         repository.save(b1);
         repository.save(b2);
+        repository.save(b3);
+        repository.save(b4);
     }
 }
